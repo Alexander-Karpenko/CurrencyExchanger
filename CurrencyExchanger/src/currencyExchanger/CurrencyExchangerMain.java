fe55c5a6d4e9501d83b3b19790b8983d2c464c9f
Alexander-Karpenko <128425416+Alexander-Karpenko@users.noreply.github.com>
2023-10-17 18:34:14 +0300

    Update CurrencyExchangerMain.java

--- CurrencyExchanger/src/currencyExchanger/CurrencyExchangerMain.java
+++ CurrencyExchanger/src/currencyExchanger/CurrencyExchangerMain.java
@@ -19,7 +19,7 @@ public class CurrencyExchangerMain  {
 	public static void main(String[] args) {
 		System.out.println("git test");
 		System.out.println("commit 2");
-		System.out.println("redact from Githab");
+		System.out.println("redact from Githab2");
 		CurrencyExchangerMetods CurrExchang = new CurrencyExchangerMetods(Currency.DOLLARS, Currency.DOLLARS);
 //		CurrExchang.setInputValue(1);
 //		CurrExchang.Exchange();
